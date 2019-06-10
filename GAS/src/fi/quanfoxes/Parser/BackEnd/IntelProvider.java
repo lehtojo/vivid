26e70b0b892708c39e0b57d1ca1a444e453c922a
GabidalG <41587318+Gabidal@users.noreply.github.com>
2019-06-10 23:28:31 +0300

    Parser

--- GAS/src/fi/quanfoxes/Parser/BackEnd/IntelProvider.java
+++ GAS/src/fi/quanfoxes/Parser/BackEnd/IntelProvider.java
@@ -1,13 +1,14 @@
 package fi.quanfoxes.Parser.BackEnd;
 
+import fi.quanfoxes.Lexer.NumberToken;
 import fi.quanfoxes.Lexer.Token;
 import fi.quanfoxes.Lexer.TokenType;
 
 import java.util.List;
 
 public class IntelProvider {
-    public List<Variables> variables;
-    public Intel_backend backend = new Intel_backend();
+    public List<Variable> variables;
+    public Intell_Backend backend = new Intell_Backend();
     public String opcode = " ";
     public String Repeater = " ";
 
@@ -26,7 +27,7 @@ public class IntelProvider {
     {
         for (int i = 0; i < tokens.size(); i++)
         {
-            if (tokens.get(i).getType() == TokenType.DATA_TYPE)
+            if (tokens.get(i).getType() == TokenType.TYPE)
             {
                 if (tokens.get(i).getText().equals("num"))
                 {
@@ -35,7 +36,7 @@ public class IntelProvider {
                 if (tokens.get(i+1).getType() == TokenType.VARIABLE)
                 {
                     boolean exists = false;
-                    int value;
+                    int value = 0;
                     for (int j = 0; j < variables.size(); j++)
                     {
                         if (tokens.get(i+1).getText().equals(variables.get(j)))
@@ -53,33 +54,89 @@ public class IntelProvider {
                             {
                                 if (tokens.get(i+4).getType() == TokenType.OPERATOR)
                                 {
-                                    if (tokens.get(i+4).getText().equals("+") || tokens.get(i+4).getText().equals("-") || tokens.get(i+4).getText().equals("/") || tokens.get(i+4).getText().equals("*"))
+                                    if (tokens.get(i+4).getText().equals("+"))
                                     {
-
+                                        if (tokens.get(i+5).getType() == TokenType.NUMBER)
+                                        {
+                                            NumberToken Left = (NumberToken) tokens.get(i+3);
+                                            NumberToken Right = (NumberToken) tokens.get(i+5);
+                                            value = Left.getNumber().intValue() + Right.getNumber().intValue();
+                                        }
+                                    }
+                                    if (tokens.get(i+4).getText().equals("-"))
+                                    {
+                                        if (tokens.get(i+5).getType() == TokenType.NUMBER)
+                                        {
+                                            NumberToken Left = (NumberToken) tokens.get(i+3);
+                                            NumberToken Right = (NumberToken) tokens.get(i+5);
+                                            value = Left.getNumber().intValue() - Right.getNumber().intValue();
+                                        }
+                                    }
+                                    if (tokens.get(i+4).getText().equals("/"))
+                                    {
+                                        if (tokens.get(i+5).getType() == TokenType.NUMBER)
+                                        {
+                                            NumberToken Left = (NumberToken) tokens.get(i+3);
+                                            NumberToken Right = (NumberToken) tokens.get(i+5);
+                                            value = Left.getNumber().intValue() / Right.getNumber().intValue();
+                                        }
+                                    }
+                                    if (tokens.get(i+4).getText().equals("*"))
+                                    {
+                                        if (tokens.get(i+5).getType() == TokenType.NUMBER)
+                                        {
+                                            NumberToken Left = (NumberToken) tokens.get(i+3);
+                                            NumberToken Right = (NumberToken) tokens.get(i+5);
+                                            value = Left.getNumber().intValue() * Right.getNumber().intValue();
+                                        }
                                     }
                                 }
                             }
                             if (tokens.get(i+3).getType() == TokenType.VARIABLE)
                             {
-
+                                boolean exist2 = false;
+                                int value2;
+                                Right_immediate = true;
+                                int x = 0;
+                                for (int j = 0; j < variables.size(); j++)
+                                {
+                                    if (tokens.get(i+3).getText().equals(variables.get(j)))
+                                    {
+                                        value2 = variables.get(j).value;
+                                        exist2 = true;
+                                        Source = "ebp";
+                                        x = variables.get(j).X;
+                                    }
+                                    if (exist2)
+                                    {
+                                        Right_Math = "-";
+                                        Right_Number = String.valueOf(Math.abs(x));
+                                    }
+                                    else
+                                    {
+                                        String error = String.format("  G::Error %d doesn´t seem to exist´s", variables.get(j).name);
+                                        System.out.println(error);
+                                    }
+                                }
                             }
-                        }
-                        if (tokens.get(i+2).getText().equals("++"))
-                        {
-                            opcode = "add";
-                        }
-                        if (tokens.get(i+3).getType() == TokenType.NUMBER)
-                        {
-
-                        }
-                        if (tokens.get(i+3).getType() == TokenType.VARIABLE)
-                        {
-
                         }
                     }
                     if (exists)
                     {
-
+                        variables.get(i).value = value;
+                    }
+                    else
+                    {
+                        Variable v = new Variable();
+                        if (variables.size() == 0)
+                        {
+                            v.X = 0;
+                        }
+                        else
+                        {
+                            v.X = variables.get(variables.size() - 1).X + 1;
+                        }
+                        variables.add(v);
                     }
                 }
             }
---
+++ GAS/src/fi/quanfoxes/Parser/BackEnd/Variable.java
@@ -0,0 +1,7 @@
+package fi.quanfoxes.Parser.BackEnd;
+
+public class Variable {
+    public String name;
+    public int value;
+    public int X; //the offset in the stack frame.
+}
