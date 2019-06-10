56bcda35fdd68441d5d989895203598b755f21e8
GabidalG <41587318+Gabidal@users.noreply.github.com>
2019-06-10 23:29:00 +0300

    Delete Variables.java

--- GAS/src/fi/quanfoxes/Parser/BackEnd/Variables.java
+++
@@ -1,8 +0,0 @@
-package fi.quanfoxes.Parser.BackEnd;
-
-public class Variables {
-    public String name;
-    public int value;
-    public int Y; //the stack frame that the variable is in.
-    public int X; //the offset in the stack frame.
-}
