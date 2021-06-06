bdab96683956b76e9539bbdaf45bc769a5b282e8
Joonas Lehto <joonas.eemil.lehto@gmail.com>
2021-06-06 19:58:40 +0300

    Empty parenthesis in functions will now generate an error (function calls without arguments and empty statements are excluded)

--- Vivid/Parser/Nodes/ContentNode.cs
+++ Vivid/Parser/Nodes/ContentNode.cs
@@ -1,4 +1,4 @@
-public class ContentNode : Node
+public class ContentNode : Node, IResolvable
 {
 	public ContentNode(Position? position = null)
 	{
@@ -6,8 +6,19 @@ public class ContentNode : Node
 		Instance = NodeType.CONTENT;
 	}
 
+	public Node? Resolve(Context context)
+	{
+		foreach (var node in this) Resolver.Resolve(context, node);
+		return null;
+	}
+
 	public override Type? TryGetType()
 	{
 		return First?.TryGetType();
+	}
+
+	public Status GetStatus()
+	{
+		return First != null ? Status.OK : Status.Error(Position, "Empty parenthesis are not allowed");
 	}
 }
